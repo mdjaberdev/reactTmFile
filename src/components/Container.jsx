53f2a5fc0ac1dfe35d3e156a77a2c1addff5c3e9
Md Jaber Hossain <md9292463@gmail.com>
2026-02-25 12:39:21 +0600

    style: Container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,8 @@
 const Container = ({ className, children }) => {
-  return <div className={`max-w-330 mx-auto ${className}`}>{children}</div>;
+  return 
+  (
+    <div className={`max-w-330 mx-auto ${className}`}>{children}</div>
+  );
 };
 
 export default Container;
