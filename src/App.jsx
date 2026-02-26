2f7032feb7e37d55303a70cc43f2107b58877dd1
mdjaberdev <md9292463@gmail.com>
2026-02-26 14:51:33 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
+import { useRef, useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import RootLayout from "./components/pages/layouts/RootLayout";
-import Home from "./components/pages/Home";
 
 function App() {
+  let [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow(!show);
+  };
+
+  let containerRef = useRef(false);
+  let handleShowTwo = () => {
+    if (containerRef == "block") {
+      containerRef.current.hidden
+    }else {
+       containerRef.current
+    }
+  };
   return (
-    <>
-      <>
-        <Routes>
-          <Route path="/" element={<RootLayout />}>
-            <Route index element={<Home />} />
-          </Route>
-        </Routes>
-      </>
-    </>
+    <div className="flex justify-around">
+      {/* useState  */}
+      <div className="">
+        <button onClick={handleShow}>Open</button>
+        <div>
+          {show && (
+            <ul className="">
+              <li>Home</li>
+              <li>About</li>
+              <li>Blog</li>
+              <li>Contact</li>
+            </ul>
+          )}
+        </div>
+      </div>
+      <div className="">
+        <button onClick={handleShowTwo}>OpenTwo</button>
+        <ul ref={containerRef} className={`hidden`}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
